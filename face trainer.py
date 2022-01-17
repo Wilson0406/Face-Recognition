a4329e2ff0fd89bac2678fd7d8213f5ccb5e4aa5
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-01-17 23:01:19 +0530

    Add files via upload

---
+++ face trainer.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+from PIL import Image
+import os
+
+
+# In[2]:
+
+
+# Path for image database
+path = 'dataset'
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+
+
+# In[3]:
+
+
+# function to get the images and label data
+def getImagesAndLabels(path):
+
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
+    faceSamples=[]
+    ids = []
+
+    for imagePath in imagePaths:
+
+        PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+        img_numpy = np.array(PIL_img,'uint8')
+
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces = detector.detectMultiScale(img_numpy)
+
+        for (x,y,w,h) in faces:
+            faceSamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
+
+    return faceSamples,ids
+
+print ("\nTraining faces. It will take a few seconds. Wait ...")
+faces,ids = getImagesAndLabels(path)
+recognizer.train(faces, np.array(ids))
+
+# Save the model into trainer/trainer.yml
+recognizer.write('trainer/trainer.yml')
+
+# Print the number of faces trained and end program
+print("\n{0} faces trained. Exiting Program".format(len(np.unique(ids))))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
